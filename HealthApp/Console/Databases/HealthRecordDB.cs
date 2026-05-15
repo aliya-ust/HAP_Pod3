f7c63756f2ffc8b0ce7a6ec62a03babb0618e1b1
Aliya Shameer <310057@ust.com>
2026-05-16 01:20:35 +0530

    Created DatabaseLayer

---
+++ HealthApp/Console/Databases/HealthRecordDB.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using HealthcareApp.Models;
+
+namespace HealthcareApp.Data
+{
+    public static class Database
+    {
+        public static List<HealthRecord> Records = new List<HealthRecord>();
+    }
+}
