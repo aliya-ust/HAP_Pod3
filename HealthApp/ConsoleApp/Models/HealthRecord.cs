dbc4577a54828d65a97d8426903beaa2ca5b94b9
Rijo Biju Abraham(UST <310466@ust.com>
2026-05-18 12:38:59 +0530

    update menu

--- HealthApp/ConsoleApp/Models/HealthRecord.cs
+++ HealthApp/ConsoleApp/Models/HealthRecord.cs
@@ -14,5 +14,10 @@ namespace HealthApp.ConsoleApp.Models
         {
             return $"Patient: {Patient} | Doctor: {Doctor} | Date: {VisitDate.ToShortDateString()} | Diagnosis: {Diagnosis} | Prescription: {Prescription} | Notes: {DoctorNotes}";
         }
+
+        public override string ToString()
+        {
+            return $"Record Id: {RecordId} | Patient: {Patient} | Doctor: {Doctor} | Visit Date: {VisitDate.ToShortDateString()} | Diagnosis: {Diagnosis} | Prescription: {Prescription} | Doctor Notes: {DoctorNotes}";
+        }
     }
 }
