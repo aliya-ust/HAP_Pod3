3a8011660be7101602f6339ff60cbc64a6bed017
Sailesh Thammali(UST <291494@ust.com>
2026-05-18 17:39:40 +0530

    Reapply "Added appointments interface & Repo"
    This reverts commit dbc3f19d4cd0a4c9a2c33898af3a72400daaf2b3.

--- HealthApp/ConsoleApp/Models/Patient.cs
+++ HealthApp/ConsoleApp/Models/Patient.cs
@@ -1,5 +1,5 @@
 using System;
-namespace HealthApp.Console.Models
+namespace HealthApp.ConsoleApp.Models
 {
     public class Patient
     {
@@ -12,10 +12,10 @@ namespace HealthApp.Console.Models
         public int InsuranceId { get; set; }
         public DateTime CreatedAt { get; set; }
 
-        public Patient()
-        {
-            CreatedAt = DateTime.Now;
-        }
+        // public Patient()
+        // {
+        //     CreatedAt = DateTime.Now;
+        // }
 
         public int GetAge()
         {
