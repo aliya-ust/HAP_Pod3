f18129f3310672f34c58500d27eddbcb9cb64916
Rijo Biju Abraham(UST <310466@ust.com>
2026-06-05 16:00:24 +0530

    doctor controller

---
+++ Desktop/Learning/HealthCareApi/HealthCareApi/Controllers/DoctorController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using HealthCareApi.Repositories.Interfaces;
+
+namespace HealthCareApi.Controllers
+{
+    [RoutePrefix("api/doctors")]
+    public class DoctorController : ApiController
+    {
+        private readonly IDoctorService _doctorService;
+
+        public DoctorController(IDoctorService doctorService)
+        {
+            _doctorService = doctorService;
+        }
+
+        [HttpGet, Route("")]
+        public IHttpActionResult GetAll() =>
+        Ok(_doctorService.GetAllDoctors());
+    }
+}
