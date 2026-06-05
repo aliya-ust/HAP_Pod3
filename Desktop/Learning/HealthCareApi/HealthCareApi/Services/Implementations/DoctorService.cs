5c98085afc1b5e473814ff60d07e5e2b8a805e38
Rijo Biju Abraham(UST <310466@ust.com>
2026-06-05 16:02:32 +0530

    doctor service

---
+++ Desktop/Learning/HealthCareApi/HealthCareApi/Services/Implementations/DoctorService.cs
@@ -0,0 +1,24 @@
+﻿using HealthCareApi.Data.Repositories.Interfaces;
+using HealthCareApi.Service.Interfaces;
+using HealthCareApi.Models;
+using System.Collections.Generic;
+
+namespace HealthCareApi.Service.Implementations
+{
+    public class DoctorService : IDoctorService
+    {
+
+        private readonly IDoctorRepository _doctorRepository;
+
+        public DoctorService(IDoctorRepository doctorRepository)
+        {
+            _doctorRepository = doctorRepository;
+        }
+
+        public List<Doctor> GetAllDoctors()
+        {
+            return _doctorRepository.GetAll();
+        }
+
+    }
+}
---
+++ Desktop/Learning/HealthCareApi/HealthCareApi/Services/Interfaces/IDoctorService.cs
@@ -0,0 +1,11 @@
+﻿using HealthCareApi.Models;
+using System.Collections.Generic;
+
+namespace HealthCareApi.Service.Interfaces
+{
+    public interface IDoctorService
+    {
+        List<Doctor> GetAllDoctors();
+    }
+
+}
