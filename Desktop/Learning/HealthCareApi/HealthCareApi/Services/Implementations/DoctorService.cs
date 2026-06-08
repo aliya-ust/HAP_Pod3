059ff029c6b2ea5a79f0c197d50b8a2f693cfceb
Rijo Biju Abraham(UST <310466@ust.com>
2026-06-08 14:46:35 +0530

    doctor service

--- Desktop/Learning/HealthCareApi/HealthCareApi/Services/Implementations/DoctorService.cs
+++ Desktop/Learning/HealthCareApi/HealthCareApi/Services/Implementations/DoctorService.cs
@@ -2,12 +2,12 @@
 using HealthCareApi.Services.Interfaces;
 using HealthCareApi.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HealthCareApi.Services.Implementations
 {
     public class DoctorService : IDoctorService
     {
-
         private readonly IDoctorRepository _doctorRepository;
 
         public DoctorService(IDoctorRepository doctorRepository)
@@ -15,10 +15,48 @@ namespace HealthCareApi.Services.Implementations
             _doctorRepository = doctorRepository;
         }
 
-        public List<Doctor> GetAllDoctors()
+        public async Task<IEnumerable<Doctor>> GetAllDoctorsAsync()
         {
-            return _doctorRepository.GetAll();
+            return await _doctorRepository.GetAllAsync();
         }
 
+        public async Task<Doctor> GetDoctorByIdAsync(int id)
+        {
+            return await _doctorRepository.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Doctor>> GetFilteredDoctorsAsync(
+            string specialization = null,
+            string searchTerm = null,
+            bool orderByDescending = false,
+            int pageNumber = 1,
+            int pageSize = 10)
+        {
+            return await _doctorRepository.GetDoctorsAsync(
+                specialization,
+                searchTerm,
+                orderByDescending,
+                pageNumber,
+                pageSize);
+        }
+
+        public async Task AddDoctorAsync(Doctor doctor)
+        {
+            await _doctorRepository.AddAsync(doctor);
+        }
+
+        public async Task UpdateDoctorAsync(Doctor doctor)
+        {
+            await _doctorRepository.UpdateAsync(doctor);
+        }
+
+        public async Task DeleteDoctorAsync(int id)
+        {
+            var doctor = await _doctorRepository.GetByIdAsync(id);
+            if (doctor != null)
+            {
+                await _doctorRepository.DeleteAsync(doctor);
+            }
+        }
     }
 }
--- Desktop/Learning/HealthCareApi/HealthCareApi/Services/Interfaces/IDoctorService.cs
+++ Desktop/Learning/HealthCareApi/HealthCareApi/Services/Interfaces/IDoctorService.cs
@@ -1,11 +1,22 @@
 ﻿using HealthCareApi.Models;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace HealthCareApi.Services.Interfaces
 {
     public interface IDoctorService
     {
-        List<Doctor> GetAllDoctors();
-    }
+        Task<IEnumerable<Doctor>> GetAllDoctorsAsync();
+        Task<Doctor> GetDoctorByIdAsync(int id);
+        Task<IEnumerable<Doctor>> GetFilteredDoctorsAsync(
+            string specialization = null,
+            string searchTerm = null,
+            bool orderByDescending = false,
+            int pageNumber = 1,
+            int pageSize = 10);
 
+        Task AddDoctorAsync(Doctor doctor);
+        Task UpdateDoctorAsync(Doctor doctor);
+        Task DeleteDoctorAsync(int id);
+    }
 }
