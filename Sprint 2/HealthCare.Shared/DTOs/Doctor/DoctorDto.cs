36a1c02e28fc051772dd56558b7d9a302e4a6288
Suhana Ali <310508@ust.com>
2026-06-11 04:42:23 +0530

    Initial Commit

---
+++ Sprint 2/HealthCare.Shared/DTOs/Doctor/DoctorDto.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthCare.Shared.DTOs.Doctor
+{
+    public class DoctorDto
+    {
+        public int DoctorId { get; set; }
+        public string FullName { get; set; }
+        public string Specialisation { get; set; }
+        public int YearsOfExperience { get; set; }
+        public decimal ConsultationFee { get; set; }
+        public bool IsActive { get; set; }
+    }
+} 
