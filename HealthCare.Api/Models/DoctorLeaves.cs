5a5ecb1a5cd96e65a7562dab911fd13356ea9a5d
Rijo Biju <310466@ust.com>
2026-06-15 15:47:48 +0530

    doctor slots and leave models

---
+++ HealthCare.Api/Models/DoctorLeaves.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthCare.Api.Models
+{
+    public class DoctorLeaves // Doctor leave model for doctor with necessary constraints
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int DoctorId { get; set; }
+
+        [Required]
+        public DateOnly LeaveDate { get; set; }
+
+        [MaxLength(500)]
+        public string? Reason { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        [ForeignKey(nameof(DoctorId))]
+        public Doctor Doctor { get; set; } = null!;
+    }
+}
