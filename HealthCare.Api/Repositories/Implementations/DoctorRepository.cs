b7b0fde277a3713c553fc1b37d0735e0412edce3
Rijo Biju <310466@ust.com>
2026-06-15 23:17:48 +0530

    doctor repository

---
+++ HealthCare.Api/Repositories/Implementations/DoctorRepository.cs
@@ -0,0 +1,11 @@
+﻿using HealthCare.Api.Data;
+using HealthCare.Api.Models;
+using HealthCare.Api.Repositories.Interfaces;
+
+namespace HealthCare.Api.Repositories.Implementations
+{
+    public class DoctorRepository : Repository<Doctor>, IDoctorRepository
+    {
+        public DoctorRepository(HealthCareDbContext context) : base(context) { }
+    }
+}
---
+++ HealthCare.Api/Repositories/Interfaces/IDoctorRepository.cs
@@ -0,0 +1,8 @@
+﻿using HealthCare.Api.Models;
+
+namespace HealthCare.Api.Repositories.Interfaces
+{
+    public interface IDoctorRepository : IRepository<Doctor>
+    {
+    }
+}
