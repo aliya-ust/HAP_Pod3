c339039c753162a1be72cfc49a5a300432b94db8
Rijo Biju <310466@ust.com>
2026-06-15 23:35:50 +0530

    doctor service

---
+++ HealthCare.Api/Services/Implementations/DoctorService.cs
@@ -0,0 +1,42 @@
+﻿using HealthCare.Api.Data;
+using HealthCare.Api.Models;
+using HealthCare.Api.Repositories.Interfaces;
+using HealthCare.Api.Services.Interfaces;
+
+namespace HealthApp.Infrastructure.Services;
+
+public class DoctorService : IDoctorService
+{
+    private readonly IRepository<Doctor> _repository;
+    private readonly HealthCareDbContext _context;
+
+    public DoctorService(IRepository<Doctor> repository, HealthCareDbContext context)
+    {
+        _repository = repository;
+        _context = context;
+    }
+
+    public async Task<Doctor?> GetByIdAsync(int id) =>
+        await _repository.GetByIdAsync(id);
+
+    public async Task<IEnumerable<Doctor>> GetAllAsync() =>
+        await _repository.GetAllAsync();
+
+    public async Task AddAsync(Doctor doctor)
+    {
+        await _repository.AddAsync(doctor);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task UpdateAsync(Doctor doctor)
+    {
+        await _repository.UpdateAsync(doctor);
+        await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteAsync(int id)
+    {
+        await _repository.DeleteAsync(id);
+        await _context.SaveChangesAsync();
+    }
+}
---
+++ HealthCare.Api/Services/Interfaces/IDoctorService.cs
@@ -0,0 +1,13 @@
+﻿using HealthCare.Api.Models;
+
+namespace HealthCare.Api.Services.Interfaces
+{
+    public interface IDoctorService
+    {
+        Task<Doctor?> GetByIdAsync(int id);
+        Task<IEnumerable<Doctor>> GetAllAsync();
+        Task AddAsync(Doctor doctor);
+        Task UpdateAsync(Doctor doctor);
+        Task DeleteAsync(int id);
+    }
+}
