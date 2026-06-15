7eef59e9e78482304b10a498a6d227d09f576670
Rijo Biju <310466@ust.com>
2026-06-16 00:03:20 +0530

    health record dtos

---
+++ HealthCare.Api/DTOs/HealthRecord/UpdateHealthRecord.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthCare.Api.DTOs.HealthRecord
+{
+    public class UpdateHealthRecord
+    {
+        [Required]
+        [MaxLength(500)]
+        public string Diagnosis { get; set; } = null!;
+
+        [Required]
+        [MaxLength(500)]
+        public string Prescription { get; set; } = null!;
+
+        [MaxLength(1000)]
+        public string? Notes { get; set; }
+    }
+}
