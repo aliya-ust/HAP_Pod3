f06f0008f7b0bc3374496c981d5248634be7c1bb
Rijo Biju <310466@ust.com>
2026-06-15 15:46:15 +0530

    health record model

--- HealthCare.Api/Models/Patient.cs
+++ HealthCare.Api/Models/Patient.cs
@@ -1,7 +1,4 @@
-﻿using HealthCare.Api.Models;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Diagnostics.HealthChecks;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HealthCare.Api.Models
