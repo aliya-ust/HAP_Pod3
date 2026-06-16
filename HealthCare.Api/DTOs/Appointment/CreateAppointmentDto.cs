371afb0a188ff0b0adb21e4967cc2b31945d5313
Rijo Biju <310466@ust.com>
2026-06-16 14:45:40 +0530

    appointment dto validation

--- HealthCare.Api/DTOs/Appointment/CreateAppointmentDto.cs
+++ HealthCare.Api/DTOs/Appointment/CreateAppointmentDto.cs
@@ -11,10 +11,27 @@ namespace HealthCare.Api.DTOs.Appointment
         public int DoctorId { get; set; }
 
         [Required]
+        [FutureDateValidation]
         public DateOnly ScheduledDate { get; set; }
 
         [Required]
         [MaxLength(20)]
         public string TimeSlot { get; set; } = null!;
+    }
+
+    public class FutureDateValidation : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext context)
+        {
+            if (value is DateOnly date)
+            {
+                if (date <= DateOnly.FromDateTime(DateTime.Today))
+                {
+                    return new ValidationResult("Scheduled date must be in the future.");
+                }
+            }
+
+            return ValidationResult.Success;
+        }
     }
 }
