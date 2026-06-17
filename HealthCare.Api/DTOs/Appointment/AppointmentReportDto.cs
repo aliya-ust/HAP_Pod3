3ee2f4847ceb3b29118504d94fd81710947fd55c
Rijo Biju <310466@ust.com>
2026-06-17 16:38:46 +0530

    appointment repo

--- HealthCare.Api/DTOs/Appointment/AppointmentReportDto.cs
+++ HealthCare.Api/DTOs/Appointment/AppointmentReportDto.cs
@@ -3,7 +3,7 @@
     public class AppointmentReportDto
     {
         public DateOnly Date { get; set; }
-
+        public int PendingCount { get; set; }
         public int ConfirmedCount { get; set; }
 
         public int CancelledCount { get; set; }
--- HealthCare.Api/Repositories/Interfaces/IAppointmentRepository.cs
+++ HealthCare.Api/Repositories/Interfaces/IAppointmentRepository.cs
@@ -1,8 +1,16 @@
-﻿using HealthCare.Api.Models;
+﻿using HealthCare.Api.DTOs.Appointment;
+using HealthCare.Api.Models;
 
 namespace HealthCare.Api.Repositories.Interfaces
 {
     public interface IAppointmentRepository : IRepository<Appointment>
     {
+        Task<List<string>> AvailableTimeSlots(DateOnly date, int doctorId);
+        Task<bool> IsAvailable(DateOnly date, int doctorId, string timeSlot);
+        Task<List<AppointmentReportDto>> GetDailyReport();
+        Task<List<AppointmentListDto>> GetDoctorSchedule(DateOnly date, int id);
+        Task<List<AppointmentListDto>> GetPatientSchedule(DateOnly date, int id);
+        Task<List<AppointmentListDto>> GetAppointmentByPatient(int id);
+        Task<List<AppointmentListDto>> GetAppointmentByDoctor(int id);
     }
 }
