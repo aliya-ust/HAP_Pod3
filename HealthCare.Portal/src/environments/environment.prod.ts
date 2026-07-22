8d8fbd09ff2e77f06c39762efe6166b98c5918dc
Rijo Biju <310466@ust.com>
2026-07-22 15:24:23 +0530

    change API port to 80 for corporate network compatibility

--- HealthCare.Portal/src/environments/environment.prod.ts
+++ HealthCare.Portal/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://3.107.74.36:8080/api',
-  adminUrl: 'http://3.107.74.36:8080',
+  apiUrl: 'http://3.107.74.36/api',
+  adminUrl: 'http://3.107.74.36',
 };
